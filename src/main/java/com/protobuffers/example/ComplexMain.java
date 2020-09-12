5a3169c332a31a96075db0660735097f66fb79e7
Rajkumar Chandrasekaran <rajkumar.chandrasekaran@cognizant.com>
2020-09-12 19:04:15 +0530

    Update ComplexMain.java

--- src/main/java/com/protobuffers/example/ComplexMain.java
+++ src/main/java/com/protobuffers/example/ComplexMain.java
@@ -1,10 +1,40 @@
 package com.protobuffers.example;
 
+import java.util.ArrayList;
+
+import example.complex.Complex.ComplexMessage;
+import example.complex.Complex.ComplexMessage.Builder;
+import example.complex.Complex.DummyMessage;
+
 public class ComplexMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Builder builder = ComplexMessage.newBuilder();
+		
+		example.complex.Complex.DummyMessage.Builder dummyBuilder = createDummyMessage("rajkumar", 123);
+		example.complex.Complex.DummyMessage.Builder dummyBuilder1 = createDummyMessage("rajkumar1", 234);
+		example.complex.Complex.DummyMessage.Builder dummyBuilder2 = createDummyMessage("rajkumar2", 456);
+		example.complex.Complex.DummyMessage.Builder dummyBuilder3 = createDummyMessage("rajkumar3", 678);
+		
+		ArrayList<example.complex.Complex.DummyMessage> dummyBuilderList = new ArrayList<example.complex.Complex.DummyMessage>();
+		dummyBuilderList.add(dummyBuilder1.build());
+		dummyBuilderList.add(dummyBuilder2.build());
+		dummyBuilderList.add(dummyBuilder3.build());
+		
+		builder.setOneDummy(dummyBuilder.build());
+		builder.addAllMultipleDummy(dummyBuilderList);
+		
+		System.out.println(builder.build());
+		
+	}
+
+	private static example.complex.Complex.DummyMessage.Builder createDummyMessage(String name, int id) {
+		example.complex.Complex.DummyMessage.Builder dummyBuilder= DummyMessage.newBuilder();
+		
+		dummyBuilder.setId(id);
+		dummyBuilder.setName(name);
+		return dummyBuilder;
 	}
 
 }
